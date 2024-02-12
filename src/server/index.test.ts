1c0c4265cf8f5bd1f5caa77bf6bff815d1bd02bb
Vitaly <vitaliy.zayakin@gmail.com>
2024-02-13 04:54:48 +0500

    feat: add scenario 2 test

--- src/server/index.test.ts
+++ src/server/index.test.ts
@@ -1,6 +1,8 @@
 import request from 'supertest';
 import { describe, expect, test } from '@jest/globals';
 import { createServer } from './createServer';
+import { validate } from 'uuid';
+import { User } from '../types';
 
 describe('Get all records with a GET api/users request (an empty array is expected)', () => {
   test('', async () => {
@@ -13,6 +15,27 @@ describe('Get all records with a GET api/users request (an empty array is expect
     expect(response.body).toStrictEqual([]);
     expect(response.statusCode).toBe(200);
     expect(response.type).toBe('application/json');
+
+    server.close();
+  });
+});
+
+describe('A new object is created by a POST api/users request (a response containing newly created record is expected)', () => {
+  test('', async () => {
+    const port = 4000;
+    const url = `http://localhost:${port}`;
+    const server = createServer(port);
+    const testUser = {
+      username: 'fake user',
+      age: 29,
+      hobbies: [],
+    };
+
+    const response = await request(url).post('/api/users/').send(testUser);
+
+    expect(response.statusCode).toBe(201);
+    expect(response.type).toBe('application/json');
+    expect(validate(response.body.id)).toBe(true);
 
     server.close();
   });
